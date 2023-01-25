a76432eb3eb9ba81382d85ec14197a8577d480f7
El1syum <kilmol606@gmail.com>
2023-01-25 22:04:32 +0300

    added two paths; 301-302 redirects; 403 handlers

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('<h2>Women</h2>')
+
+
+def categories(request, catid):
+    if request.GET:
+        print(request.GET)
+    if catid > 99:
+        return redirect('home', permanent=True)
+    return HttpResponse(f'<h2>Category {catid}</h2>')
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Page not found</h1>')
